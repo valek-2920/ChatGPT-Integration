55cc850cbc3a73228ea7eb88cb7ab3384537cdbc
Valeria Morales <78573287+valek-2920@users.noreply.github.com>
2023-12-08 07:38:32 -0600

    Update Title.tsx

--- frontend/src/components/Title.tsx
+++ frontend/src/components/Title.tsx
@@ -28,7 +28,7 @@ function Title({ setMessages }: Props) {
 
   return (
     <div className="flex justify-between items-center w-full p4 bg-gray-900 text-white font-bold shadow">
-      <div className="italic"> Sarah</div>
+      <div className="italic"> Glenda</div>
       <button
         onClick={resetConversation}
         className={
